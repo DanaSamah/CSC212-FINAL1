7e26fe1dd9d0dacdef5d6c0fe56144844c7dd508
nora221 <noraalsaif2003@gmail.com>
2023-10-09 11:44:45 +0300

    happy

--- FINAL_CSC212/src/Contact.java
+++ FINAL_CSC212/src/Contact.java
@@ -76,7 +76,7 @@ public class Contact implements comparable<String> {
 	}
 	public String toString() {
 	return "Contact [name=" + name + ", phoneNumber=" + phoneNumber + ", emailAddress=" + emailAddress
-	+ ", address=" + address + ", birthday=" + birthday + ", notes=" + notes + "]" + ",";
+	+ ", address=" + address + ", birthday=" + birthday + ", notes=" + notes + "]" ;
 	}
 	}
 	
